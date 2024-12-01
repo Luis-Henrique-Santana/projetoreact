9ca6bfc571f41c0f5be5ca589f8332a59ff99f9b
Luis-Henrique-Santana <lhs3000t@gmail.com>
2024-12-01 14:21:32 -0300

    adição de pesquisa de livros

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,11 @@
+import livro from '../../imagens/livro.png'
+import livro2 from '../../imagens/livro2.png'
+
+export const Livros = [
+    {nome: 'Livro 1', id: 1, src:livro},
+    {nome: 'Livro 2', id: 2, src:livro2},
+    {nome: 'Livro 3', id: 3, src:livro},
+    {nome: 'Livro 4', id: 0, src:livro2},
+
+]
+
